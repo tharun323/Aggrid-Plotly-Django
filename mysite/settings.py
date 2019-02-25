3d4eacbf912d78c6789b3959d4c5efaa691eddcf
tharun323 <atkr323@gmail.com>
2019-02-25 09:44:01 +0530

    Feature-charts and datatable are added

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@ge36(vy5egb2-)7cn819@ghyu97w__gjqdd7)p(y8q6qb8-o1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
